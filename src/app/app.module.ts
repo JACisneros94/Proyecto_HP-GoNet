fe98af95cf984dc26a7acf44dc933a73ab17c27a
JACisneros94 <jacisnerosp.94@gmail.com>
2021-11-27 12:09:26 -0600

    Inicializacion del proyecto, se crearon componentes a trabajar y estructura HTML principal, se inicio a estilizar y agregaron archivos en assets descargados de Figma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FiltersComponent } from './filters/filters.component';
+import { CardsComponent } from './cards/cards.component';
+import { CardComponent } from './card/card.component';
+import { FavListComponent } from './fav-list/fav-list.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FiltersComponent,
+    CardsComponent,
+    CardComponent,
+    FavListComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
