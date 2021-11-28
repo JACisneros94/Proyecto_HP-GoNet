4ad5dc462ffd000f1db2b6827c48824e4e57ce95
JACisneros94 <jacisnerosp.94@gmail.com>
2021-11-28 00:36:53 -0600

    Se agregó propiedad NAME en los inputs del Modal, se creo carpeta MODELS y asi mismo el model Character

---
+++ src/app/models/Character.ts
@@ -0,0 +1,12 @@
+export default interface Character {
+  id: number;
+  name: string;
+  house: string;
+  alive: boolean;
+  eyeColour: string;
+  hairColour: string;
+  image: string;
+  hogwartsStudent: boolean;
+  hogwartsStaff: boolean;
+  gender: string;
+};
