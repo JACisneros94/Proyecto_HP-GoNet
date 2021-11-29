31679b3cc187269a6cb6365ce4dbbe1ba8b800a4
JACisneros94 <jacisnerosp.94@gmail.com>
2021-11-28 22:28:15 -0600

    Se realizaron ligeros cambios en estos archivos

--- src/app/models/Character.ts
+++ src/app/models/Character.ts
@@ -1,11 +1,12 @@
 export default interface Character {
   id: number;
   name: string;
-  house: string;
+  house: 'Gryffindor'|'Ravenclaw'|'Hufflepuff'|'Slytherin';
   alive: boolean;
   eyeColour: string;
   hairColour: string;
   image: string;
+  dateOfBirth: string;
   hogwartsStudent: boolean;
   hogwartsStaff: boolean;
   gender: string;
