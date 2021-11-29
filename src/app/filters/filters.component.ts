5caa0f5da529a2312cf7f5c58bfc2f428105b9aa
JACisneros94 <jacisnerosp.94@gmail.com>
2021-11-28 22:25:01 -0600

    Se creo funcionalidad para realizar el filtro de Estudiantes o Staff

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from '../state.service';
 
 @Component({
   selector: 'app-filters',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filters.component.scss']
 })
 export class FiltersComponent implements OnInit {
+  
+  activeFilter = '';
 
-  constructor() { }
+  constructor(private stateService: StateService) { }
 
   ngOnInit(): void {
+    this.stateService.activeFilter$.subscribe(activeFilter => this.activeFilter = activeFilter);
+  }
+
+  toggleFilter(filter: string) {
+    this.stateService.updateActiveFilter(this.activeFilter == filter ? '' : filter);
+  }
+
+  filterIsActive(filter: string): boolean {
+    return this.activeFilter === filter;
   }
 
 }
